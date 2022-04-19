f1b99b75baa46949d3a0a7990b3a2a0ccd2253a2
elridion <hans@goedeke.org>
2022-04-19 16:17:57 +0200

    Update mix.exs

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,1 @@
-use Mix.Config
-
-config :mete, tags: [user: "hans"]
+import Config
--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Mete.MixProject do
     [
       app: :mete,
       version: @version,
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
@@ -23,7 +23,7 @@ defmodule Mete.MixProject do
   def application do
     [
       mod: {Mete.Application, []},
-      extra_applications: [:logger]
+      extra_applications: [:logger, :inets, :runtime_tools]
     ]
   end
 
